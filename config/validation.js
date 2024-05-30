84d8e6bea0aae69a6319dd8ef122c16664ee2db6
Patrick <patrickcerkalb@gmail.com>
2024-05-30 15:55:37 +0200

    throw error

--- config/validation.js
+++ config/validation.js
@@ -19,10 +19,7 @@ const registerValidation = data => {
 
     const { error } = schema.validate(data);
     if (error) {
-        return {
-            success: false,
-            message: error.details[0].message
-        };
+        throw new Error(error.details[0].message);
     }
 
     return {
@@ -38,10 +35,7 @@ const loginValidation = data => {
 
     const { error } = schema.validate(data);
     if (error) {
-        return {
-            success: false,
-            message: error.details[0].message
-        };
+        throw new Error(error.details[0].message);
     }
 
     return {
