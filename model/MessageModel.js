388a798ef5ded57c768e3a30991525151c081765
Matteo Frigo <114284526+FrigoMatteo@users.noreply.github.com>
2024-06-20 18:50:06 +0200

    Update of message model

--- model/MessageModel.js
+++ model/MessageModel.js
@@ -2,26 +2,43 @@ const mongoose = require('mongoose');
 const moment = require('moment-timezone');
 
 const messageSchema = mongoose.Schema({
-    message: {
+    clientA: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6,
+        maxlength: 255
     },
-    sender: {
+    clientB: {
         type: String,
-        required: true
+        required: true,
+        minlength: 6,
+        maxlength: 255
     },
-    receiver: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: String,
-        default: function() {
-            var time = moment.tz(new Date(), "Europe/Rome");
-            return time.format('YYYY/MM/DD HH:mm');
+    messages: [new mongoose.Schema({
+        sender: {
+            type: String,
+            required: true,
+            minlength: 6,
+            maxlength: 255
+        },
+        text: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 1024,
+        },
+        date: {
+            type: String,
+            default: function () {
+                var time = moment.tz(new Date(), "Europe/Rome");
+                return time.format('YYYY/MM/DD HH:mm');
+            }
         }
-    }
+    }, {
+        _id: false // Opzione per evitare di creare un _id per ogni elemento in messages
+    })],
 });
+
 
 const Message = mongoose.model('Message', messageSchema);
 
