7e7cd0f6db5f7257086656b15da8995bc4409180
Matteo Frigo <114284526+FrigoMatteo@users.noreply.github.com>
2024-06-20 18:49:23 +0200

    Update of message

--- routes/messages.js
+++ routes/messages.js
@@ -5,38 +5,79 @@ const verify = require('../config/verifyToken');
 const Message = require('../model/MessageModel');
 
 router.post('/', verify, async (req, res) => {
-    const { message, receiver } = req.body; // renamed Message to messageContent and Receiver to receiver
-    const sender = req.user._id; 
+    const user = await User.findById(req.user);
+
+    if (!user){
+        return res.status(400).json({ status: 400, message: 'User not found' });
+    }
+
+    const receiver = req.body.receiver;
+
+    const message=req.body.message;
+
+    let [first, second] = [user.email, receiver].sort((a, b) => a.localeCompare(b));
 
     try {
-        const newMessage = new Message({ 
-            message: message,
-            sender: sender, 
-            receiver: receiver
-        });
+        mes=await Message.findOne({clientA:first, clientB:second});
 
-        const savedMessage = await newMessage.save();
 
-        res.status(200).json(savedMessage);
+        if (mes){
+            const newMessage={
+                sender:user.email,
+                text:message,
+            }
+
+            mes.messages.push(newMessage);
+
+            await mes.save();
+            res.status(200).json({status:200,message:"sent"});
+        }else{
+            const newChat=new Message({
+                clientA:first,
+                clientB:second,
+                messages:[
+                    {
+                        sender:user.email,
+                        text:message,
+                    }
+                ]
+            });
+
+            await newChat.save();
+            res.status(200).json({status:200,message:"sent"});
+        }
     } catch (error) {
         console.error('Error creating message', error);
         res.status(500).json({ status: 500, message: 'Internal Server Error' });
     }
 });
 
-router.get('/', verify, async (req, res) => {
+router.get('/:UserEmail', verify, async (req, res) => {
+    const user = await User.findById(req.user);
+
+    if (!user){
+        return res.status(400).json({ status: 400, message: 'User not found' });
+    }
+
+    otherUser=req.params.UserEmail;
+
+    let [first, second] = [user.email, otherUser].sort((a, b) => a.localeCompare(b));
+
     try {
-        const messages = await Message.find({
-            $or: [
-                { receiver: req.user._id },
-                { sender: req.user._id }
-            ]
-        }).select('message -_id');
-    
-        res.status(200).json(messages);
+        mes=await Message.findOne({clientA:first, clientB:second});
+
+        if (mes){
+
+            return res.status(200).json({status:200,messages:mes.messages});
+
+        }else{
+
+            return res.status(200).json({status:200,messages:[]});
+
+        }
     } catch (error) {
         console.error('Error getting messages', error);
-        res.status(500).json({ status: 500, message: 'Internal Server Error' });
+        return res.status(500).json({ status: 500, message: 'Internal Server Error' });
     }
 });
 
