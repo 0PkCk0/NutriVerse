f32f5385bd06ee584a163f4e64c1663104a521f3
Patrick <patrickcerkalb@gmail.com>
2024-06-21 16:49:48 +0200

    delete subscirbers con if lunghezza

--- routes/ProUser.js
+++ routes/ProUser.js
@@ -5,7 +5,7 @@ const verify = require('../config/verifyToken');
 const router = require('express').Router();
 const moment = require('moment-timezone');
 const mongoose = require('mongoose');
-const {sendUpdateUser}=require('../config/updateUser');
+const { sendUpdateUser } = require('../config/updateUser');
 
 const getFormattedDate = function () {
     var time = moment.tz(new Date(), "Europe/Rome");
@@ -22,16 +22,16 @@ router.post('/', verify, async (req, res) => {
     try {
         const basicUser = await User.findById(req.user._id);
 
-        if (!basicUser) return res.status(400).send({code:400, message:'User not found'});
+        if (!basicUser) return res.status(400).send({ code: 400, message: 'User not found' });
 
-        if (req.body.Profession==="" || !req.body.Profession){
-            return res.status(400).send({code:400, message:'Empty body Profession'});
+        if (req.body.Profession === "" || !req.body.Profession) {
+            return res.status(400).send({ code: 400, message: 'Empty body Profession' });
         }
 
         // Check if the user is already a ProUser
 
         if (basicUser.Profession && (basicUser.Profession.includes('Nutritionist') || basicUser.Profession.includes('Personal Trainer'))) {
-            return res.status(400).send({code:400, message:'User is already a Professionist and cannot update'});
+            return res.status(400).send({ code: 400, message: 'User is already a Professionist and cannot update' });
         }
 
         // Prepare the update data
@@ -62,15 +62,15 @@ router.post('/', verify, async (req, res) => {
         }
 
         if (!updatedUser) {
-            return res.status(400).send({code:400, message:'Error updating user to ProUser'});
+            return res.status(400).send({ code: 400, message: 'Error updating user to ProUser' });
         }
 
         // Generate a new token
         const token = jwt.sign({ _id: updatedUser._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
-        res.status(201).header('auth-token', token).send({code:201, token:token});
+        res.status(201).header('auth-token', token).send({ code: 201, token: token });
     } catch (err) {
         console.error('Error:', err);
-        res.status(400).send({code:400, message:err});
+        res.status(400).send({ code: 400, message: err });
     }
 });
 
@@ -81,23 +81,25 @@ router.delete('/', verify, async (req, res) => {
 
         const subscribersId = updatedProUser.subscribersId; // This should be fetched from the professional's document
 
-        subscribersId.forEach(async (userEmail) => {
-            try {
-                // Find the user by their email
-                const user = await User.findOne({ email: userEmail });
-                if (user) {
-                    // Assuming the user has a field 'subscriptions' which is an array of professional emails
-                    await User.findByIdAndUpdate(user._id, { $pull: { subscriptions: requester.email } });
+        if (subscribersId && subscribersId.length > 0) {
+            subscribersId.forEach(async (userEmail) => {
+                try {
+                    // Find the user by their email
+                    const user = await User.findOne({ email: userEmail });
+                    if (user) {
+                        // Assuming the user has a field 'subscriptions' which is an array of professional emails
+                        await User.findByIdAndUpdate(user._id, { $pull: { subscriptions: requester.email } });
+                    }
+                } catch (error) {
+                    console.error(`Error updating user ${userEmail}: ${error}`);
+                    // Handle error appropriately, maybe accumulate errors to respond with or log them
                 }
-            } catch (error) {
-                console.error(`Error updating user ${userEmail}: ${error}`);
-                // Handle error appropriately, maybe accumulate errors to respond with or log them
-            }
-        });
+            });
+        }
 
-        if (!proUser) return res.status(400).json({code :400, message:'User not found'});
+        if (!proUser) return res.status(400).json({ code: 400, message: 'User not found' });
         if (proUser.userType === 'User') {
-            return res.status(400).json({code :400, message:'User is not a ProUser'});
+            return res.status(400).json({ code: 400, message: 'User is not a ProUser' });
         }
 
         let updates = {};
@@ -115,7 +117,7 @@ router.delete('/', verify, async (req, res) => {
                 gender: proUser.gender,
                 subscriptionsId: proUser.subscriptionsId,
                 userType: 'User',
-                confirmed:true,
+                confirmed: true,
                 __t: 'User',
                 timestamp: proUser.timestamp
             });
@@ -126,7 +128,7 @@ router.delete('/', verify, async (req, res) => {
 
             await newUser.save();
             const token = jwt.sign({ _id: newUser._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
-            res.status(201).json({code:201, token:token});
+            res.status(201).json({ code: 201, token: token });
 
         } else if (['Nutritionist', 'Personal Trainer'].includes(proUser.Profession)) {
             updates = {
@@ -153,7 +155,7 @@ router.delete('/', verify, async (req, res) => {
         }
 
         if (!updatedUser) {
-            return res.status(400).json({code:400,message:'Error updating user'});
+            return res.status(400).json({ code: 400, message: 'Error updating user' });
         }
 
         // Hydrate the document to ensure it conforms to the User model schema
@@ -162,11 +164,11 @@ router.delete('/', verify, async (req, res) => {
 
         // Generate a new token
         const token = jwt.sign({ _id: updatedUser._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
-        res.status(201).header('auth-token', token).json({code:201, token:token});
+        res.status(201).header('auth-token', token).json({ code: 201, token: token });
     } catch (err) {
         console.error('Error:', err);
         if (!res.headersSent) {
-            res.status(400).json({code:400, message:err.message || 'An error occurred'});
+            res.status(400).json({ code: 400, message: err.message || 'An error occurred' });
         }
     }
 });
@@ -183,14 +185,14 @@ router.get('/:userID', verify, async (req, res) => {
     const userID = req.params.userID;
 
     //Check if we get the subscriptionId in the request parameters
-    if (!userID){
+    if (!userID) {
         return res.status(404).json({ message: 'Id not found' });
     }
 
     //Check if it is one of our subscriber
-    for (const id of user.subscribersId){
-        if (id===userID){
-            return await sendUpdateUser(res,userID);
+    for (const id of user.subscribersId) {
+        if (id === userID) {
+            return await sendUpdateUser(res, userID);
         }
     }
 
@@ -226,50 +228,50 @@ router.get('/', verify, async (req, res) => {
 
     // Populate subscribers array
     for (const mail of user.subscribersId) {
-        const userSub_query = await User.findOne({email: mail});
+        const userSub_query = await User.findOne({ email: mail });
         const userSub = userSub_query;
         let insert_push = {};
-        
-        if(userSub.name){
+
+        if (userSub.name) {
             insert_push.name = userSub.name;
         }
         insert_push.email = userSub.email;
-    
+
         if (userSub.Profession === 'Nutritionist') {
             insert_push.profession = 'N'
-        } else if(userSub.Profession === 'Personal Trainer') {
+        } else if (userSub.Profession === 'Personal Trainer') {
             insert_push.profession = 'P'
         } else {
             insert_push.profession = 'B'
         }
-    
+
         // Index for selecting the user image in the main dashboard
         insert_push.index = 1;
         insert_push.code = userSub.Code;
-    
+
         resultJSON.subscribers.push(insert_push);
     }
-    
+
     // Populate requests array
     for (const mail of user.requestId) {
         const userReq_query = await User.findOne({ email: mail });
         const userReq = userReq_query;
         let insert_push = {};
-    
+
         insert_push.email = userReq.email;
-    
+
         if (userReq.Profession === 'Nutritionist') {
             insert_push.profession = 'N'
-        } else if(userReq.Profession === 'Personal Trainer') {
+        } else if (userReq.Profession === 'Personal Trainer') {
             insert_push.profession = 'P'
         } else {
             insert_push.profession = 'B'
         }
-    
+
         // Index for selecting the user image in the main dashboard
         insert_push.index = 1;
         insert_push.code = userReq.Code;
-    
+
         resultJSON.requests.push(insert_push);
     }
 
