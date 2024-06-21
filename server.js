cc55e399aed2580bf35a416df2b20d968a8bbaaf
FrigoMatteo <matteo.frigo-4@studenti.unitn.it>
2024-06-21 15:47:40 +0200

    Fixed server file for using the socket

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const http = require("http");
 const app = require("./index");
+const socketio = require('socket.io');
+const cors = require("cors"); // Importa cors
+
+
+app.use(cors());
 const server = http.createServer(app);
 
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', "true");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
 });
+
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("joinRoom", ({ user1, user2 }) => {
+        console.log(user1);
+        console.log(user2);
+        const room = [user1, user2].sort().join("_");
+        socket.join(room);
+        console.log(`User joined room: ${room}`);
+    });
+
+    socket.on("sendMessage", (message) => {
+        const room = [message.senderId, message.receiverId].sort().join("_");
+        console.log(message.payload);
+        io.to(room).emit("receiveMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
 // server listening
 app.listen(3000, () => {
     console.log(`Server running on port 3000`);
