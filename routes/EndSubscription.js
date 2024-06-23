e46c4e41933595caf5c960e540230de591e6b585
Patrick <patrickcerkalb@gmail.com>
2024-06-23 17:39:27 +0200

    get end sub

--- routes/EndSubscription.js
+++ routes/EndSubscription.js
@@ -5,13 +5,12 @@ const app = express();
 const verify = require("../config/verifyToken");
 
 app.get('/', verify, async (req, res) => {
-    const date = moment();
-    const prouser = await ProUser.findById(req.user);
 
-    const date1 = moment.tz(date, "Europe/Rome");
+    const prouser = await ProUser.findById(req.user);
+    const date = moment.tz("Europe/Rome").format("YYYY-MM-DD HH:mm");
     const date2 = moment.tz(prouser, "YYYY-MM-DD HH:mm", "Europe/Rome");
 
-    const isDate1AfterDate2 = date1.isAfter(date2);
+    const isDate1AfterDate2 = date.isAfter(date2);
 
     if (isDate1AfterDate2) {
         return res.status(200).json({ status: 200, message: 'Subscription ended' });
